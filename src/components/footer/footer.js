e6cf390094748b29a093d1a4cb1d526cb6a7f5a3
sriuthaya004-byte <sriuthaya004@gmail.com>
2026-09-19 08:56:22 +0300

    Create footer.js

---
+++ src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>Movie Store</div>
+      <div>&copy;2022 , Movie Inc. or it's affiliates</div>
+    </div>
+  );
+};
+
+export default Footer;
